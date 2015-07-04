a5eb87a523fd5123fef4485428c9b15694854e7e
Moritz Pflanzer <mp3414@ic.ac.uk>
2015-07-04 13:53:01 +0100

    Added universal start script for creduce

---
+++ startReduction.py
@@ -0,0 +1,71 @@
+#!/usr/bin/env python3
+
+import argparse, os, sys, subprocess
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description='Start C-Reduce for OpenCL kernel.')
+    parser.add_argument('--verbose', '-v', action='store_true', help='Verbose output')
+    parser.add_argument('--platform', '-p', help='Platform under test')
+    parser.add_argument('--device', '-d', help='Device under test')
+    parser.add_argument('--cl-launcher', help='Path to cl_launcher application')
+    parser.add_argument('--clang', help='Path to clang application')
+    parser.add_argument('--libclc', help='Path to libclc include directory')
+    if sys.platform == 'win32':
+        parser.add_argument('--oclgrind-platform', help='Platform for Oclgrind')
+        parser.add_argument('--oclgrind-device', help='Device for Oclgrind')
+    parser.add_argument('test', narg=1, help='Test script')
+    parser.add_argument('kernel', narg=1, help='OpenCL kernel')
+
+    args = parser.parse_args()
+
+    env = os.environ
+
+    if not args.platform:
+        if not env.get('CREDUCE_TEST_PLATFORM'):
+            parser.error('No platform specified and CREDUCE_TEST_PLATFORM not defined!')
+    else:
+        env['CREDUCE_TEST_PLATFORM'] = args.platform
+
+    if not args.testDevice:
+        if not env.get('CREDUCE_TEST_DEVICE'):
+            parser.error('No device specified and CREDUCE_TEST_DEVICE not defined!')
+    else:
+        env['CREDUCE_TEST_DEVICE'] = args.device
+
+    if not args.cl_launcher:
+        if not env.get('CREDUCE_TEST_CLLAUNCHER'):
+            parser.error('No cl-launcher specified and CREDUCE_TEST_CLLAUNCHER not defined!')
+    else:
+        env['CREDUCE_TEST_CLLAUNCHER'] = args.cl_launcher
+
+    if args.clang:
+        env['CREDUCE_TEST_CLANG'] = args.clang
+
+    if not args.libclc:
+        if not env.get('CREDUCE_TEST_LIBCLC_INCLUDE_PATH'):
+            parser.error('No libclc specified and CREDUCE_TEST_LIBCLC_INCLUDE_PATH not defined!')
+    else:
+        env['CREDUCE_TEST_LIBCLC_INCLUDE_PATH'] = args.libclc
+
+    if sys.platform == 'win32':
+        if not args.oclgrind_platform:
+            if not env.get('CREDUCE_TEST_OCLGRIND_PLATFORM'):
+                parser.error('No oclgrind-platform specified and CREDUCE_TEST_OCLGRIND_PLATFORM not defined!')
+        else:
+            env['CREDUCE_TEST_OCLGRIND_PLATFORM'] = args.oclgrind_platform
+
+        if not args.oclgrind_device:
+            if not env.get('CREDUCE_TEST_OCLGRIND_DEVICE'):
+                parser.error('No oclgrind-device specified and CREDUCE_TEST_OCLGRIND_DEVICE not defined!')
+        else:
+            env['CREDUCE_TEST_OCLGRIND_DEVICE'] = args.oclgrind_device
+
+    creduceArgs = ['creduce', '-n', '1']
+
+    if args.verbose:
+        creduceArgs.append('--verbose')
+
+    creduceArgs.append(args.test)
+    creduceArgs.append(args.kernel)
+
+    subprocess.call(creduceArgs, env=env, universal_newlines=True)
---
+++ unix/set_test_env.sh
@@ -0,0 +1,13 @@
+export CREDUCE_TEST_CLANG=/vol/bitbucket/mp3414/install/bin/clang
+export CREDUCE_TEST_CLLAUNCHER=/homes/mp3414/msc_project/clsmith/cl_launcher
+export CREDUCE_TEST_LIBCLC_INCLUDE_PATH=/vol/bitbucket/mp3414/libclc/generic/include
+
+export CLSMITH_PATH=/homes/mp3414/msc_project/clsmith
+
+export CUDA_VISIBLE_DEVICES=1
+
+export CREDUCE_TEST_PLATFORM=0
+export CREDUCE_TEST_DEVICE=0
+
+#export CREDUCE_TEST_DEBUG=1
+#export CREDUCE_TEST_LOG=1
